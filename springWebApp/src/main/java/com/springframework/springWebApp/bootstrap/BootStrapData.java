57299a00b6a5ce9a16a680b5e59faeb2acf02f96
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-03-03 16:35:13 -0600

    created boostrap data to verify output

--- springWebApp/src/main/java/com/springframework/springWebApp/bootstrap/BootStrapData.java
+++ springWebApp/src/main/java/com/springframework/springWebApp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.springframework.springWebApp.bootstrap;
 
 import com.springframework.springWebApp.domain.Author;
 import com.springframework.springWebApp.domain.Book;
+import com.springframework.springWebApp.domain.Publisher;
 import com.springframework.springWebApp.repositories.AuthorRepository;
 import com.springframework.springWebApp.repositories.BookRepository;
+import com.springframework.springWebApp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,27 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setAddress("");
+        publisher.setCity("St. Petersburg");
+        publisher.setState("NYC");
+        publisher.setZip(112251);
+
+        publisherRepository.save(publisher);
+        System.out.println("Started in Bootstrap");
+        System.out.println("Publisher Count: " + publisherRepository.count());
 
         Author colleen = new Author("Colleen" ,"Hoover");
         Book bk = new Book("Reminders of Him","123123");
@@ -36,7 +51,7 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(eric);
         bookRepository.save(bk1);
 
-        System.out.println("Started in Bootstrap");
+
         System.out.println("Number of Books:" + bookRepository.count());
 
 
