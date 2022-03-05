226073bb80d797fa3246f01ffffb57cae8bea46f
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-03-05 13:05:10 -0600

    implemented equals and hashcode

--- springWebApp/src/main/java/com/springframework/springWebApp/domain/Author.java
+++ springWebApp/src/main/java/com/springframework/springWebApp/domain/Author.java
@@ -55,5 +55,18 @@ public class Author {
         this.books = books;
     }
 
-   
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
