9089850f1e4db1296a56468175f7e61b2fc36344
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-03-05 13:57:10 -0600

    implemeting many to many relationship

--- springWebApp/src/main/java/com/springframework/springWebApp/domain/Book.java
+++ springWebApp/src/main/java/com/springframework/springWebApp/domain/Book.java
@@ -1,9 +1,6 @@
 package com.springframework.springWebApp.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.Set;
 
 @Entity
@@ -14,6 +11,9 @@ public class Book {
 
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name="book_id"),
+            inverseJoinColumns = @JoinColumn(name="author_id"))
     private Set<Author> authors;
 
     public Book() {
